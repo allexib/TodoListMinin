b739fb649168763f83a68c033e8583ef6923de25
allexib <ooomegasib@gmail.com>
2021-02-06 16:30:31 +0400

    9 useEffect, api

--- src/App.js
+++ src/App.js
@@ -1,14 +1,21 @@
-import React from 'react'
+import React, {useEffect} from 'react'
 import TodoList from './Todo/TodoList'
 import Context from './context'
 import AddTodo from './AddTodo'
 
 function App() {
-    const [todos, setTodos] = React.useState([
-        {id: 1, completed: false, title: 'Купить хлеб'},
-        {id: 2, completed: true, title: 'Купить масло'},
-        {id: 3, completed: false, title: 'Купить молоко'}
-    ])
+    const [todos, setTodos] = React.useState([])
+
+    useEffect(() => {
+        fetch('https://jsonplaceholder.typicode.com/todos?_limit=5')
+            .then(response => response.json())
+            .then(todos => {
+                setTimeout(() => {
+                    setTodos(todos)
+                }, 2000)
+
+            })
+    }, [])
 
     function toggleTodo(id) {
         setTodos

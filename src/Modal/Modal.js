651d75f3192f70e00636be731de436bf00a4e444
allexib <ooomegasib@gmail.com>
2021-02-06 17:33:06 +0400

    12.1 react.fragment, modal, 1 miss

--- src/Modal/Modal.js
+++ src/Modal/Modal.js
@@ -3,22 +3,26 @@ import './Modal.css'
 
 export default class Modal extends React.Component {
     state = {
-       isOpen: false
+        isOpen: false
     }
 
     render() {
         return (
             <React.Fragment>
-                <button onClick={() => this.setState({isOpen: true})}>Open modal</button>
+                <button onClick={() => this.setState({isOpen: true})}>
+                    Open modal
+                </button>
 
                 {this.state.isOpen && (
                     <div className='modal'>
-                    <div className='modal-body'>
-                        <h1>Modal title</h1>
-                        <p>I am awesome modal</p>
-                        <button onClick={() => this.setState({isOpen: false})}>Close modal</button>
+                        <div className='modal-body'>
+                            <h1>Modal title</h1>
+                            <p>I am awesome modal</p>
+                            <button onClick={() => this.setState({isOpen: false})}>
+                                Close modal
+                            </button>
+                        </div>
                     </div>
-                </div>
                 )}
             </React.Fragment>
         )

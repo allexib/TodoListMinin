adfd325713e95f4e90d5076f1263c6d943923c31
allexib <ooomegasib@gmail.com>
2021-02-06 17:04:46 +0400

    12 react.fragment, modal

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import TodoList from './Todo/TodoList'
 import Context from './context'
 
 import Loader from './Loader'
+import Modal from "./Modal/Modal";
 
 const AddTodo = React.lazy(() => new Promise(resolve => {
     setTimeout(() => {
@@ -53,6 +54,8 @@ function App() {
         <Context.Provider value={{removeTodo}}>
             <div className='wrapper'>
                 <h1>React tutorial</h1>
+                <Modal/>
+
                 <React.Suspense fallback={<Loader/>}>
                     <AddTodo onCreate={addTodo}/>
                 </React.Suspense>
